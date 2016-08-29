29a8f57d3721b6aa5dd0fea3b94de07795117a57
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-08-29 16:43:25 +0700

    Default

---
+++ PenguinPairs/DefaultsManager.swift
@@ -0,0 +1,80 @@
+//
+//  DefaultsManager.swift
+//  PenguinPairs
+//
+//  Created by MacMini on 8/29/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import SpriteKit
+
+class DefaultsManager{
+    
+    static let instance = DefaultsManager()
+    var defaults = NSUserDefaults.standardUserDefaults()
+    
+    init(){
+        if(defaults.arrayForKey("levelStatus") != nil){
+            return
+        }
+        
+        let filePath = NSBundle.mainBundle().pathForResource("defaults", ofType: "plist")
+        let defaultPreferences = NSDictionary(contentsOfFile:filePath!)!
+        
+        for(key,value) in defaultPreferences{
+            defaults.setValue(value, forKey: key as! String)
+        }
+    }
+    
+    func reset(){
+        let filePath = NSBundle.mainBundle().pathForResource("defaults", ofType: "plist")
+        let defaultPreferences = NSDictionary(contentsOfFile: filePath!)!
+        
+        for (key,_) in defaultPreferences{
+            defaults.removeObjectForKey(key as! String)
+        }
+    }
+    
+    var musicVolume : Float{
+        get{
+            return defaults.floatForKey("backgroundMusicVolume")
+        }
+        
+        set{
+            defaults.setFloat(newValue, forKey: "backgroundMusicVolume")
+        }
+    }
+    
+    var hints : Bool{
+        get{
+            return defaults.boolForKey("hints")
+        }
+        
+        set{
+            defaults.setBool(newValue, forKey: "hints")
+        }
+    }
+    
+    func getLevelStatus (levelNr : Int) -> String{
+        let levels = defaults.arrayForKey("levelStatus")
+        
+        if levelNr < 1 || levelNr > levels?.count{
+            return "locked"
+        }
+        else{
+            return levels?[levelNr-1] as! String
+        }
+    }
+    
+    func setLevelStatu(levelNr : Int, status : String){
+        var levels = defaults.arrayForKey("levelStatus")
+        
+        if levelNr < 1 || levelNr > levels?.count{
+            return
+        }
+        else{
+            levels![levelNr - 1] = status
+            defaults.setObject(levels, forKey: "levelStatus")
+        }
+    }
+}
